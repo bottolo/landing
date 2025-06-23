6ebf6e4610c0422de449fee36f0b1ae2713623fa
bottolo <samuele.castiglia.98@gmail.com>
2025-06-23 14:44:05 +0200

    feat: add scrollbar hide

--- apps/btl/src/routes/games/$choice.tsx
+++ apps/btl/src/routes/games/$choice.tsx
@@ -15,7 +15,8 @@ function RouteComponent() {
 			<div
 				className={cn(
 					"absolute flex flex-col items-center gap-24 bottom-8 opacity-20 h-[85vh] w-[300px] md:w-[700px] overflow-hidden scale-80 transition-all duration-800",
-					view === "images" && "overflow-y-auto z-[1] opacity-100 scale-100",
+					view === "images" &&
+						"overflow-y-auto scrollbar-hide z-[1] opacity-100 scale-100",
 				)}
 			>
 				<img className={"w-[700px]"} src={"/img.png"} />
@@ -30,7 +31,7 @@ function RouteComponent() {
 				className={cn(
 					"absolute flex flex-col items-center w-[250px] md:w-[700px] opacity-20 text-center text-2xl/14 h-[85vh] gap-[10rem] bottom-8 overflow-hidden scale-80 transition-all duration-800",
 					view === "text" &&
-						"overflow-y-auto z-[1] opacity-100 scale-100 bg-black/30",
+						"overflow-y-auto scrollbar-hide z-[1] opacity-100 scale-100 bg-black/30",
 				)}
 			>
 				<p>
--- apps/btl/src/routes/games/index.tsx
+++ apps/btl/src/routes/games/index.tsx
@@ -55,7 +55,7 @@ const GAMES: DetailsNavigationElement[] = [
 
 function RouteComponent() {
 	return (
-		<div className="absolute max-h-[90vh] md:max-h-[92vh] bottom-2 w-full overflow-y-auto flex flex-col gap-16 items-center">
+		<div className="absolute max-h-[90vh] md:max-h-[92vh] bottom-2 w-full overflow-y-auto flex flex-col gap-16 items-center scrollbar-hide">
 			{GAMES.map((element) => (
 				<DitherCard key={element.title} element={element} />
 			))}
