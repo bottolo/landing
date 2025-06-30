5c06d98b8b603cb2e3a46cd1fbd3d323a544e2d5
bottolo <samuele.castiglia.98@gmail.com>
2025-06-30 15:45:25 +0200

    feat: add games images

--- apps/btl/src/data/games-content-data.tsx
+++ apps/btl/src/data/games-content-data.tsx
@@ -1,12 +1,17 @@
+import { Link } from "@tanstack/react-router";
 import type { ReactNode } from "react";
 
 export const GAMES_CONTENT_DATA: {
 	[key: string]: {
+		title?: string;
+		link?: string;
 		paragraphs: ReactNode[];
 		images: string[];
 	};
 } = {
 	noclip: {
+		title: "noclip",
+		link: "/games/noclip",
 		paragraphs: [
 			<p>
 				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
@@ -50,15 +55,19 @@ export const GAMES_CONTENT_DATA: {
 			</p>,
 		],
 		images: [
-			"/img_1.png",
-			"/img_2.png",
-			"/img_3.png",
-			"/img_4.png",
-			"/img_5.png",
-			"/img_6.png",
+			"/games/noclip/ncp_0.png",
+			"/games/noclip/ncp_1.png",
+			"/games/noclip/ncp_2.png",
+			"/games/noclip/ncp_3.png",
+			"/games/noclip/ncp_4.png",
+			"/games/noclip/ncp_5.png",
+			"/games/noclip/ncp_6.png",
+			"/games/noclip/ncp_7.png",
 		],
 	},
 	octant: {
+		title: "octant",
+		link: "/games/octant",
 		paragraphs: [
 			<p>
 				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
@@ -102,15 +111,23 @@ export const GAMES_CONTENT_DATA: {
 			</p>,
 		],
 		images: [
-			"/img_1.png",
-			"/img_2.png",
-			"/img_3.png",
-			"/img_4.png",
-			"/img_5.png",
-			"/img_6.png",
+			"/games/octant/oct_0.png",
+			"/games/octant/oct_1.png",
+			"/games/octant/oct_2.png",
+			"/games/octant/oct_3.png",
+			"/games/octant/oct_4.png",
+			"/games/octant/oct_5.png",
+			"/games/octant/oct_6.png",
+			"/games/octant/oct_7.png",
+			"/games/octant/oct_8.png",
+			"/games/octant/oct_9.png",
+			"/games/octant/oct_10.png",
+			"/games/octant/oct_11.png",
 		],
 	},
-	apus: {
+	phony: {
+		title: "phony",
+		link: "/games/phony",
 		paragraphs: [
 			<p>
 				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
@@ -154,30 +171,34 @@ export const GAMES_CONTENT_DATA: {
 			</p>,
 		],
 		images: [
-			"/img_1.png",
-			"/img_2.png",
-			"/img_3.png",
-			"/img_4.png",
-			"/img_5.png",
-			"/img_6.png",
+			"/games/phony/ph_0.png",
+			"/games/phony/ph_1.png",
+			"/games/phony/ph_2.png",
+			"/games/phony/ph_3.png",
+			"/games/phony/ph_4.png",
+			"/games/phony/ph_5.png",
 		],
 	},
 	visio: {
 		paragraphs: [
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum.
+				visio is a project that i've made for the virtual reality course at the
+				university of milan during 2023. it is a 3d VR puzzle game made with{" "}
+				<a href={"https://unity.com/"}> unity </a> and developed with the aid of
+				the{" "}
+				<a href={"https://www.meta.com/it/en/quest/products/quest-2/"}>
+					meta quest 2
+				</a>{" "}
+				where you have to connect rays of light to the respective inputs in
+				order to proceed
 			</p>,
+
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				designed in collaboration with{" "}
+				<a href={"https://github.com/azazellus"}>azazello</a> after developing
+				<Link to={"/games/noclip"}>noclip</Link>, it features low-poly, basic
+				spherical shapes and an ethereal landscape which is always a common
+				element in init.Sense's projects
 			</p>,
 			<p>
 				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
@@ -206,12 +227,12 @@ export const GAMES_CONTENT_DATA: {
 			</p>,
 		],
 		images: [
-			"/img_1.png",
-			"/img_2.png",
-			"/img_3.png",
-			"/img_4.png",
-			"/img_5.png",
-			"/img_6.png",
+			"/games/visio/vis_0.png",
+			"/games/visio/vis_1.png",
+			"/games/visio/vis_2.png",
+			"/games/visio/vis_3.png",
+			"/games/visio/vis_4.png",
+			"/games/visio/vis_5.png",
 		],
 	},
 };
--- apps/btl/src/data/web-content-data.tsx
+++ apps/btl/src/data/web-content-data.tsx
@@ -2,51 +2,54 @@ import type { ReactNode } from "react";
 
 export const APPS_CONTENT_DATA: {
 	[key: string]: {
+		title: string;
+		link: string;
 		paragraphs: ReactNode[];
 		images: string[];
 	};
 } = {
 	landing: {
+		title: "landing",
+		link: "/apps/landing",
 		paragraphs: [
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum.
+				my personal landing page: a project which showcases how i approach web
+				development both creatively and technically
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				it is a monorepo developed with <a href={"https://react.dev/"}>react</a>
+				, <a href={"https://www.typescriptlang.org/"}>typescript</a>, and{" "}
+				<a href={"https://tailwindcss.com/"}>tailwind</a>, built with{" "}
+				<a href={"https://turborepo.com/"} target={"_blank"} rel="noopener">
+					turborepo
+				</a>{" "}
+				and deployed on <a href={"https://vercel.com/"}>vercel</a>
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing. Ex in eu magna magna
-				sunt occaecat amet deserunt ullamco laborum aliqua ea deserunt esse
-				anim. Ex sit do ex incididunt fugiat et. Eu excepteur ex eu enim commodo
-				Lorem veniam deserunt fugiat dolore officia amet enim sunt laborum.
-				Pariatur consectetur anim qui magna quis voluptate laborum quis ea minim
-				commodo aliquip tempor et. Aliquip officia cillum elit qui ipsum aute
-				consequat labore culpa incididunt qui voluptate nisi. Dolore ex anim
-				consectetur est occaecat duis adipisicing.
+				the idea behind this tech stack for a landing page comes from the need
+				to have a template for landing pages so that i can share my work if
+				anyone is interested in it
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				while the landing page is not a complex application, it serves more as a
+				manifesto for my current visual values and how i approach creative
+				development
+			</p>,
+			<p>
+				i'm aware that a site like this is not the best example of accessibility
+				or that it doesn't adhere to ui/ux standards, but the idea is to express
+				how sacrificing some well-known and respected practices can lead to a
+				more unique experience
+			</p>,
+			<p>
+				if you want to learn more about unconventional designs, take a look at{" "}
+				<a
+					href={
+						"https://books.google.it/books/about/72_dpi.html?id=ZBh0QgAACAAJ&redir_esc=y"
+					}
+				>
+					this cool book
+				</a>{" "}
 			</p>,
 		],
 		images: [
@@ -59,46 +62,43 @@ export const APPS_CONTENT_DATA: {
 		],
 	},
 	cccd: {
+		title: "cccd",
+		link: "/apps/cccd",
 		paragraphs: [
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum.
+				a project that i've worked on in the first half of 2025: it's built on
+				top of <a href={"https://react.dev/"}>react </a>
+				and <a href={"https://www.typescriptlang.org/"}>typescript</a> and it is
+				a full-stack application with no database service, deployed on{" "}
+				<a href={"https://fly.io/"}>fly.io</a>
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				it relies on google docs, google sheet and{" "}
+				<a href={"https://www.zotero.org/"}>zotero</a>'s api in order to
+				retrieve the catalog; <a href={"https://bun.sh/"}>bun</a> is used to
+				serve the application and <a href={"https://hono.dev/"}>hono</a> is used
+				to route the apis
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing. Ex in eu magna magna
-				sunt occaecat amet deserunt ullamco laborum aliqua ea deserunt esse
-				anim. Ex sit do ex incididunt fugiat et. Eu excepteur ex eu enim commodo
-				Lorem veniam deserunt fugiat dolore officia amet enim sunt laborum.
-				Pariatur consectetur anim qui magna quis voluptate laborum quis ea minim
-				commodo aliquip tempor et. Aliquip officia cillum elit qui ipsum aute
-				consequat labore culpa incididunt qui voluptate nisi. Dolore ex anim
-				consectetur est occaecat duis adipisicing.
+				below the preface as written by samuele anzellotti, the author of the
+				project
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				"born out of a shared library on, climate change in communication cesign
+				index is a desk research project on global warming issues related to the
+				field of communication design, with a particular focus on book
+				publishing
+			</p>,
+			<p>
+				addressed to young and old researchers, designers and academics, cccd is
+				a work in progress repository facilitated by samuele anzellotti during
+				his ms's research thesis in communication design at the school of
+				politecnico in Milan.
+			</p>,
+			<p>
+				climate change, sustainability and regenerative design are huge and
+				extremely complex topics, that’s why the results we show in the website
+				cannot be exhaustive"
 			</p>,
 		],
 		images: [
@@ -111,98 +111,28 @@ export const APPS_CONTENT_DATA: {
 		],
 	},
 	mediation: {
+		title: "mediation",
+		link: "/apps/mediation",
 		paragraphs: [
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum.
+				funny that i've decided to include this website in my own, but it's
+				still built on top of <a href={"https://react.dev/"}>react </a>
+				and <a href={"https://www.typescriptlang.org/"}>typescript</a> and it is
+				a full-stack application with no database service, deployed on{" "}
+				<a href={"https://fly.io/"}>fly.io</a>
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				a landing page designed for my father! it's very simple, but it
+				showcases a design drastically different from the one used here to prove
+				that, apart from creative manifestos, standard ui/ux practices are still
+				my thing. it's not entirely my work, as{" "}
+				<a href={"https://github.com/FrankieBortot"}>frankie</a> designed the
+				website
 			</p>,
 			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing. Ex in eu magna magna
-				sunt occaecat amet deserunt ullamco laborum aliqua ea deserunt esse
-				anim. Ex sit do ex incididunt fugiat et. Eu excepteur ex eu enim commodo
-				Lorem veniam deserunt fugiat dolore officia amet enim sunt laborum.
-				Pariatur consectetur anim qui magna quis voluptate laborum quis ea minim
-				commodo aliquip tempor et. Aliquip officia cillum elit qui ipsum aute
-				consequat labore culpa incididunt qui voluptate nisi. Dolore ex anim
-				consectetur est occaecat duis adipisicing.
-			</p>,
-			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
-			</p>,
-		],
-		images: [
-			"/img_1.png",
-			"/img_2.png",
-			"/img_3.png",
-			"/img_4.png",
-			"/img_5.png",
-			"/img_6.png",
-		],
-	},
-	wild: {
-		paragraphs: [
-			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum.
-			</p>,
-			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
-			</p>,
-			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing. Ex in eu magna magna
-				sunt occaecat amet deserunt ullamco laborum aliqua ea deserunt esse
-				anim. Ex sit do ex incididunt fugiat et. Eu excepteur ex eu enim commodo
-				Lorem veniam deserunt fugiat dolore officia amet enim sunt laborum.
-				Pariatur consectetur anim qui magna quis voluptate laborum quis ea minim
-				commodo aliquip tempor et. Aliquip officia cillum elit qui ipsum aute
-				consequat labore culpa incididunt qui voluptate nisi. Dolore ex anim
-				consectetur est occaecat duis adipisicing.
-			</p>,
-			<p>
-				Ex in eu magna magna sunt occaecat amet deserunt ullamco laborum aliqua
-				ea deserunt esse anim. Ex sit do ex incididunt fugiat et. Eu excepteur
-				ex eu enim commodo Lorem veniam deserunt fugiat dolore officia amet enim
-				sunt laborum. Pariatur consectetur anim qui magna quis voluptate laborum
-				quis ea minim commodo aliquip tempor et. Aliquip officia cillum elit qui
-				ipsum aute consequat labore culpa incididunt qui voluptate nisi. Dolore
-				ex anim consectetur est occaecat duis adipisicing.
+				it relies on google calendar apis to fetch events and show my father's
+				availability for appointments while giving him some freedom with mega's
+				apis to change some of the site's content
 			</p>,
 		],
 		images: [
